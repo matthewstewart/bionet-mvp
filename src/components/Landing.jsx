0c8ec75bbabf26e44bac91f3251dad16a0612afc
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-11-05 18:52:09 -0800

    update landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -25,48 +25,92 @@ class Landing extends React.Component {
         </Link>
       )
     }) : [];
+    const labsRequested = isLoggedIn ? currentUser.labsRequested.map((lab, index) => {
+      return (
+        <Link 
+          key={shortid.generate()}
+          className="list-group-item list-group-item-action bg-info text-light rounded-0"
+          to={`/labs/${lab._id}`}
+        >
+          <i className="mdi mdi-teach mr-2"/>{lab.name}
+        </Link>
+      )
+    }) : []; 
+    const labsToJoin = isLoggedIn ? currentUser.labsToJoin.map((lab, index) => {
+      return (
+        <Link 
+          key={shortid.generate()}
+          className="list-group-item list-group-item-action bg-info text-light rounded-0"
+          to={`/labs/${lab._id}`}
+        >
+          <i className="mdi mdi-teach mr-2"/>{lab.name}
+        </Link>
+      )
+    }) : [];    
     return (
       <div className="Landing container-fluid">
         
         <div className="row">
           <div className="col-12 col-lg-6">
-            <div className="card rounded-0 mt-3">
-              <div className="card-header rounded-0 bg-dark text-light">
-                <h4 className="card-title mb-0 text-capitalize">{isLoggedIn ? `Welcome Back To BioNet ${currentUser.username}` : "Welcome To BioNet"}</h4>
-              </div>
-              {(isLoggedIn) ? (
-                <>
-                  <div className="card-body">
-                    <img 
-                      src={currentUser.gravatarUrl} 
-                      className="user-img rounded d-block float-left" 
-                      alt={`${currentUser.username}`}
-                    />
-                    <div className="d-block float-left ml-3">
-                      <p className="card-text">
-                        You Currently:<br/>
-                        Belong to {currentUser.labs.length} Labs<br/>
-                        Have Requested Access to {currentUser.labsRequested.length} Labs<br/>
-                        Have {currentUser.labsToJoin.length} Labs Yet To Join
-                      </p>  
-                    </div>
+            {(isLoggedIn) ? (
+              <div className="card rounded-0 mt-3 mb-3">
+                <div className="card-header rounded-0 bg-dark text-light">
+                  <h4 className="card-title mb-0 text-capitalize">Welcome Back To BioNet {currentUser.username}</h4>
+                </div>
+            
+                  
+                <div className="card-body">
+                  <img 
+                    src={currentUser.gravatarUrl} 
+                    className="user-img rounded d-block float-left" 
+                    alt={`${currentUser.username}`}
+                  />
+                  <div className="d-block float-left ml-3">
+                    <p className="card-text">
+                      You Currently:<br/>
+                      Belong to {currentUser.labs.length} {currentUser.labs.length === 1 ? "Lab" : "Labs"}
+                    </p>  
                   </div>
-                  <ul className="list-group list-group-flush">
-                    {labsJoined}
-                    <Link
-                      to="/labs/new"
-                      className="list-group-item list-group-item-action bg-success text-light rounded-0"
-                    >
-                      <i className="mdi mdi-plus mr-2"/>Create New Lab
-                    </Link>
-                  </ul>
-                </>
-              ) : (
+                </div>
+
+                <div className="card-header rounded-0 bg-dark text-light">
+                  <h5 className="card-title mb-0 text-capitalize">Your {currentUser.labs.length === 1 ? "Lab" : "Labs"}</h5>
+                </div>                  
+                <ul className="list-group list-group-flush">
+                  {labsJoined}
+                  <Link
+                    to="/labs/new"
+                    className="list-group-item list-group-item-action bg-success text-light rounded-0"
+                  >
+                    <i className="mdi mdi-plus mr-2"/>Create New Lab
+                  </Link>
+                </ul>
+
+                <div className="card-header rounded-0 bg-dark text-light">
+                  <h5 className="card-title mb-0 text-capitalize">Pending Lab {currentUser.labsRequested.length === 1 ? "Request" : "Requests"}</h5>
+                </div>                  
+                <ul className="list-group list-group-flush">
+                  {labsRequested}
+                </ul>
+
+                <div className="card-header rounded-0 bg-dark text-light">
+                  <h5 className="card-title mb-0 text-capitalize">Other Labs</h5>
+                </div>                  
+                <ul className="list-group list-group-flush">
+                  {labsToJoin}
+                </ul>
+              
+              </div>
+            ) : (
+              <div className="card rounded-0 mt-3 mb-3">
+                <div className="card-header rounded-0 bg-dark text-light">
+                  <h4 className="card-title mb-0 text-capitalize">Welcome To BioNet</h4>
+                </div>
                 <div className="card-body">
                   <p className="card-text">Welcome To BioNet</p>
                 </div>
-              )}   
-            </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
