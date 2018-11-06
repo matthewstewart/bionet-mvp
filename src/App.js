9bddc7cdb35a6ee09995ffb5434f8d2260050164
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-11-05 17:01:05 -0800

    update landing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from "react-router-dom";
+import Crypto from 'crypto-js';
 import ErrorBoundary from './components/ErrorBoundary';
 import './App.css';
 import Loading from './components/Loading/Loading';
@@ -28,37 +29,102 @@ class App extends React.Component {
     super(props);
     this.state = {
       isLoggedIn: false,
-      currentUser: {}
+      currentUser: {},
+      labs: []
     };
     this.loginCurrentUser = this.loginCurrentUser.bind(this);
     this.logoutCurrentUser = this.logoutCurrentUser.bind(this);
     this.setCurrentUser = this.setCurrentUser.bind(this);
+    this.getCurrentUserLabs = this.getCurrentUserLabs.bind(this);
+  }
+
+  getCurrentUserLabs(currentUser) {
+    this.getLabs()
+    .then((res) => {
+      //console.log('getCurrentUserLabs.res', res);
+      let labs = res.data;
+      console.log('currentUser', currentUser);
+      //console.log('labs', labs);
+      currentUser['labs'] = [];
+      currentUser['labsRequested'] = [];
+      currentUser['labsToJoin'] = [];
+      for(let i = 0; i < labs.length; i++) {
+        let lab = labs[i];
+        let labIsJoined = false;
+        let labIsRequested = false;
+        for(let j = 0; j < lab.users.length; j++) {
+          let labUser = lab.users[j];
+          if (labUser._id === currentUser._id){
+            labIsJoined = true;
+            currentUser.labs.push(lab);
+          } 
+        }
+        for(let j = 0; j < lab.joinRequests.length; j++) {
+          let joinRequest = lab.joinRequests[j];
+          if (joinRequest._id === currentUser._id){
+            labIsRequested = true;
+            currentUser.labsRequested.push(lab);
+          }
+        }
+        if (!labIsJoined && !labIsRequested) {
+          currentUser.labsToJoin.push(lab);
+        }
+      }
+      this.setState({
+        isLoggedIn: true,
+        currentUser,
+        labs
+      });
+    });
+  }
+
+  async getLabs() {
+    try {  
+      let labsRequest = new Request(`${appConfig.apiBaseUrl}/labs`, {
+        method: 'GET',
+        headers: new Headers({
+          'Authorization': `Bearer ${Auth.getToken()}`
+        })
+      });
+      let labRes = await fetch(labsRequest);
+      let labsResponse = labRes.json();
+      return labsResponse;
+    } catch (error) {
+      console.log('App.getLabs', error);
+    }   
   }
 
   setCurrentUser() {
     //Auth.deauthenticateUser();
     if (Auth.isUserAuthenticated()) {
-      this.loginCurrentUser().then((res) => {
-        this.setState({
-          isLoggedIn: true,
-          currentUser: res.user
-        });
+      this.loginCurrentUser()
+      .then((res) => {
+        //console.log('setCurrentUser.res', res);
+        let currentUser = res.user;
+        currentUser['gravatarUrl'] = `https://www.gravatar.com/avatar/${Crypto.MD5(currentUser.email).toString()}?s=100`;
+        //console.log('currentUser: ', currentUser)
+        this.getCurrentUserLabs(currentUser);
+        // this.setState({
+        //   isLoggedIn: true,
+        //   currentUser
+        // });
+        // return currentUser;
       });
     }   
   }
 
   async loginCurrentUser() {
     try {  
-      console.log('loginCurrentUser called');
-      let request = new Request(`${appConfig.apiBaseUrl}/dashboard`, {
+      let userRequest = new Request(`${appConfig.apiBaseUrl}/dashboard`, {
         method: 'GET',
         headers: new Headers({
           'Authorization': `Bearer ${Auth.getToken()}`
         })
       });
-      let res = await fetch(request);
-      let response = res.json();
-      return response;
+      let userRes = await fetch(userRequest);
+      let userResponse = userRes.json();
+      //console.log('userResponse', userResponse);
+      return userResponse;
     } catch (error) {
       console.log('App.loginCurrentUser', error);
     } 
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import shortid from 'shortid';
 
 class Landing extends React.Component {
 
@@ -10,9 +12,63 @@ class Landing extends React.Component {
   }
 
   render() {
+    const isLoggedIn = this.props.isLoggedIn;
+    const currentUser = this.props.currentUser;
+    const labsJoined = isLoggedIn ? currentUser.labs.map((lab, index) => {
+      return (
+        <Link 
+          key={shortid.generate()}
+          className="list-group-item list-group-item-action bg-info text-light rounded-0"
+          to={`/labs/${lab._id}`}
+        >
+          <i className="mdi mdi-teach mr-2"/>{lab.name}
+        </Link>
+      )
+    }) : [];
     return (
-      <div className="Landing">
-        Is Logged In? {this.props.isLoggedIn ? JSON.stringify(this.props.currentUser) : "false"}
+      <div className="Landing container-fluid">
+        
+        <div className="row">
+          <div className="col-12 col-lg-6">
+            <div className="card rounded-0 mt-3">
+              <div className="card-header rounded-0 bg-dark text-light">
+                <h4 className="card-title mb-0 text-capitalize">{isLoggedIn ? `Welcome Back To BioNet ${currentUser.username}` : "Welcome To BioNet"}</h4>
+              </div>
+              {(isLoggedIn) ? (
+                <>
+                  <div className="card-body">
+                    <img 
+                      src={currentUser.gravatarUrl} 
+                      className="user-img rounded d-block float-left" 
+                      alt={`${currentUser.username}`}
+                    />
+                    <div className="d-block float-left ml-3">
+                      <p className="card-text">
+                        You Currently:<br/>
+                        Belong to {currentUser.labs.length} Labs<br/>
+                        Have Requested Access to {currentUser.labsRequested.length} Labs<br/>
+                        Have {currentUser.labsToJoin.length} Labs Yet To Join
+                      </p>  
+                    </div>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    {labsJoined}
+                    <Link
+                      to="/labs/new"
+                      className="list-group-item list-group-item-action bg-success text-light rounded-0"
+                    >
+                      <i className="mdi mdi-plus mr-2"/>Create New Lab
+                    </Link>
+                  </ul>
+                </>
+              ) : (
+                <div className="card-body">
+                  <p className="card-text">Welcome To BioNet</p>
+                </div>
+              )}   
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
