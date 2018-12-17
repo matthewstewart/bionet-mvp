008c57f9a8b231cd1fe1db48228396ba556ea9aa
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-12-17 13:28:25 -0800

    fix grid toolbar

--- src/components/Lab/LabProfile.jsx
+++ src/components/Lab/LabProfile.jsx
@@ -235,7 +235,8 @@ class LabProfile extends React.Component {
     let joinRequests = [];
     for(let i = 0; i < lab.users.length; i++){
       let user = lab.users[i];
-      users.push(user._id);
+      console.log('onRequestLabMembership.user', user);
+      users.push(user);
     };
     for(let i = 0; i < lab.joinRequests.length; i++){
       let request = lab.joinRequests[i];
@@ -254,7 +255,7 @@ class LabProfile extends React.Component {
     let joinRequests = [];
     for(let i = 0; i < lab.users.length; i++){
       let user = lab.users[i];
-      users.push(user._id);
+      users.push(user);
     };
     for(let i = 0; i < lab.joinRequests.length; i++){
       let request = lab.joinRequests[i];
